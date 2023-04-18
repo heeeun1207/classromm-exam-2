28a237829d2a74c61eaef3708e649e50204c19e7
jungheeeun <jungheeeun272@gmail.com>
2023-04-19 00:00:22 +0900

    Number.isInteger

---
+++ NumberisInteger.js
@@ -0,0 +1,6 @@
+// Number.isInteger(): 이 함수는 전달된 인자가 정수인지를 체크하는 내장 함수. 
+const num1 = 42;
+const num2 = 3.14;
+
+console.log(Number.isInteger(num1)); // true
+console.log(Number.isInteger(num2)); // false
